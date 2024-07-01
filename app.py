94da0de250310df13c5955ca2df249124bb327d9
Harshada Tupe <harshadatupe8@gmail.com>
2024-07-01 14:49:31 -0700

    added docker compose

--- app.py
+++ app.py
@@ -8,6 +8,62 @@ from db import stores, items
 app = Flask(__name__)
 
 
+##################
+# Store endpoints
+##################
+
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
+
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        # Here you might also want to add the items in this store
+        # We'll do that later on in the course
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload.",
+        )
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+
+    return store
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
+##################
+# Item endpoints
+##################
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
@@ -57,9 +113,6 @@ def delete_item(item_id):
 @app.put("/item/<string:item_id>")
 def update_item(item_id):
     item_data = request.get_json()
-    # There's  more validation to do here!
-    # Like making sure price is a number, and also both items are optional
-    # Difficult to do with an if statement...
     if "price" not in item_data or "name" not in item_data:
         abort(
             400,
@@ -67,58 +120,8 @@ def update_item(item_id):
         )
     try:
         item = items[item_id]
-
-        # https://blog.teclado.com/python-dictionary-merge-update-operators/
         item |= item_data
 
         return item
     except KeyError:
         abort(404, message="Item not found.")
-
-
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        # Here you might also want to add the items in this store
-        # We'll do that later on in the course
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found.")
-
-
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    if "name" not in store_data:
-        abort(
-            400,
-            message="Bad request. Ensure 'name' is included in the JSON payload.",
-        )
-    for store in stores.values():
-        if store_data["name"] == store["name"]:
-            abort(400, message=f"Store already exists.")
-
-    store_id = uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-
-    return store
-
-
-@app.delete("/store/<string:store_id>")
-def delete_store(store_id):
-    try:
-        del stores[store_id]
-        return {"message": "Store deleted."}
-    except KeyError:
-        abort(404, message="Store not found.")
-
-
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
